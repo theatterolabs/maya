89077ba4cd041b8c69fd684b9ce5411617691f81
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2022-02-16 14:02:42 +0530

    Create newdetection.js

---
+++ newdetection.js
@@ -0,0 +1,30 @@
+let detections = [];
+
+
+const videoElement = document.getElementById('video');
+
+function gotFaces(results) {
+ detections = results;
+ 
+}
+
+const faceMesh = new FaceMesh({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+}});
+faceMesh.setOptions({
+  maxNumFaces: 1,
+  selfieMode: true,
+  refineLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+faceMesh.onResults(gotFaces);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await faceMesh.send({image: videoElement});
+  },
+  width: 640,
+  height: 480
+});
+camera.start();
